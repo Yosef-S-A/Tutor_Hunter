f8e8d4dc8edd71f71003e3567709b8e810df2016
absaGH <absa3852@gmail.com>
2021-10-29 17:37:57 +0000

    database models and routes

---
+++ appmodels/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '6789d25fd941dab2cfcd9e6c02de038d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://TutorH_dev:TutorH_dev_pwd@localhost/TutorH_dev_db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+from appmodels import routes
