f3b67e39d14ac019d15e0a9fec0f70b930b9b175
absaGH <78029486+absaGH@users.noreply.github.com>
2021-11-10 17:57:54 +0300

    Update routes.py

--- appmodels/main/routes.py
+++ appmodels/main/routes.py
@@ -8,14 +8,23 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
+    """
+    A route to render the home page
+    """
     return render_template('/index3.html')
 
 @main.route("/about")
 def about():
+    """
+    A route to render the about page
+    """
     return render_template('/about.html', title="About")
 
 @main.route("/tutor_list")
 def tutor_list():
+    """
+    A route to render a page to browse tutors from
+    """
     page = request.args.get('page', 1, type=int)
     tutors = Tutor.query.paginate(page=page, per_page=3)
     return render_template('/tutor_list.html', tutors=tutors)
@@ -23,6 +32,9 @@ def tutor_list():
 @main.route("/tutor_profile/<int:tutor_id>")
 @login_required
 def tutor_profile(tutor_id):
+    """
+    A route to render the profile page of a tutor
+    """
     print('tutorhome')
     if current_user.is_authenticated and current_user.user_type == 'Tutor':
         tutor = Tutor.query.get_or_404(tutor_id)
@@ -32,7 +44,9 @@ def tutor_profile(tutor_id):
 @main.route("/tutor_list/<int:tutor_id>")
 @login_required
 def tutor_home(tutor_id):
-    print('tutorhome')
+    """
+    A route to render the a tutor's personal page
+    """
     if current_user.is_authenticated and current_user.user_type == 'Parent':
         tutor = Tutor.query.get_or_404(tutor_id)
         return render_template('tutor_home.html', title='profile', tutor=tutor)
